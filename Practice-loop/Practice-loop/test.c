9025f22cfaec51219235c7b3426c58c0078a171b
TangLing <1158354636@qq.com>
2018-10-01 22:53:58 +0800

    练习循环语句

---
+++ Practice-loop/Practice-loop/test.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+//int main(){
+//	int m = 2;
+//	int n = 2;
+//	switch (n){
+//	case 1:
+//		m++;
+//	case 2:
+//		n++;
+//	case 3:
+//		switch (m){
+//		case 1:
+//			m++;
+//		case 2 :
+//			n++;
+//		default:
+//
+//			printf("please input others");
+//		}
+//
+//		printf("%d\n", n);
+//
+//	}
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
+
+int main(){
+	char arr1[] = "welcome to bit!!";
+	char arr2[] = "################";
+	int left = 0;
+	int right = sizeof(arr1) / (arr1[0]) - 1;
+	printf("%s", arr2);
+	while (left < right){
+		arr1[]
+	}
+	system("pause");
+	return 0;
+
+}
